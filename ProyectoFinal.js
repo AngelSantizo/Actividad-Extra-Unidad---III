ea735f1ceed18c076b02a19fd72b768ad1505757
Angel Santizo <23001520@galileo.edu>
2023-02-16 20:04:06 -0600

    Primer Avance

--- ProyectoFinal.js
+++ ProyectoFinal.js
@@ -1,1 +1,24 @@
-var edad = prompt("Ingresar Edad") //este es una prueba para subir a github.
+var verirficacion = false
+var PrecioBase = 2000
+
+while(verirficacion == false){ //Preguntamos al usuario su edad para verificacion
+    var edad = parseInt(prompt("Ingresar Edad: "))
+    
+    if(edad >= 18){ //la condicion para seguir es que sea mayor de 18 
+        verirficacion = true
+    }else{
+        verirficacion = false
+        continue;
+    }
+
+    if(edad >= 18 & edad <=24){
+        PrecioBase = (PrecioBase*0.10) + PrecioBase //aca se hace el recargo del 10%  
+    }
+    if(edad >= 25 & edad <=49){
+        PrecioBase = (PrecioBase*0.20) + PrecioBase //aca del 20%
+    }
+    if(edad >= 50){
+        PrecioBase = (PrecioBase*0.30) + PrecioBase //aca del 30%
+    }
+
+}
